6237de882e6b0d6e5f0079f888faebadadd60b96
TamilSelvik0727 <111735176+TamilSelvik0727@users.noreply.github.com>
2023-03-28 16:50:51 +0530

    Add files via upload

---
+++ myProjectdemo/src/main/java/demo123/myProjectdemo/Main.java
@@ -0,0 +1,12 @@
+package demo123.myProjectdemo;
+
+	enum Subjects{
+		tamil,English,Maths,Science;
+		}
+		public class Main{
+		public static void main(String[] args){
+		Subjects s=Subjects.tamil;
+		System.out.println(s);
+		}
+		}
+
